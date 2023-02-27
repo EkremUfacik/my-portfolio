2b9fe4208e43d1c042146c5720b334249d193672
ekrem <ekremufacik07@gmail.com>
2023-02-27 23:33:58 +0300

    css

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -6,14 +6,12 @@ const About = () => {
     <div id="about" className="app__about">
       <div>
         <h2 className="app__head">About me</h2>
-        <h4>Full Stack Developer from Turkey</h4>
+        <h3>Full Stack Developer from Turkey</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti aut
-          dicta itaque culpa, dolor eius asperiores debitis eveniet odio quidem
-          quisquam natus magnam saepe mollitia. Doloribus quaerat accusamus
-          repudiandae, velit nisi, sunt labore totam voluptatibus atque
-          reiciendis eius libero dicta similique esse fuga? Impedit accusamus
-          suscipit voluptas. Accusantium, repudiandae odio.
+          I am a graduate of Istanbul University, Faculty of Law. I received
+          Full Stack Development and IT training in a Bootcamp. I have one year
+          of Full-Stack experience. I worked with React in the Front-End and
+          Python and Django in the Back-End.
         </p>
       </div>
     </div>
