35c512b473be4d62c3506173ebc28a2b0ef4a3e2
ekrem <ekremufacik07@gmail.com>
2023-03-07 22:21:49 +0300

    dark

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Footer.scss";
 import { FaLinkedinIn, FaDiscord, FaGithub } from "react-icons/fa";
-import { useThemeContext } from "../../context/ThemeProvider";
 
 const Footer = () => {
   return (
